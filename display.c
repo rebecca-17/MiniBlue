9e17efb5131d4745a9851871099296215228ce96
Arron Ye <arronye@udel.edu>
2025-05-12 23:30:03 +0000

    adjusted bounds

--- display.c
+++ display.c
@@ -80,7 +80,7 @@ void display_map(char map, int xOffset, int yOffset, char* color, int degrees, i
                 x = (i);
                 y = (j);
                 fb->bitmap->pixel[x][y] = WHITE;
-                if (Rx >= 1 && Rx < 7 && Ry >= 1 && Ry < 7) {
+                if (Rx >= 0 && Rx < 8 && Ry >= 0 && Ry < 8) {
                     fb->bitmap->pixel[Rx][Ry] = BLUE;
             }
         }
