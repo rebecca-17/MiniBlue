a667cb2438115ec9bba6396859b65853cf2e79db
Arron Ye <arronye@udel.edu>
2025-05-12 23:35:57 +0000

    added boudn logic fore rebecca

--- main.c
+++ main.c
@@ -15,16 +15,24 @@ void handler(unsigned int code) {
     printf("code: %u\t",code);
     if(code == KEY_UP) {
         //printf("right");
+        if (!(vx >= 8)) {
         vx++;
+        }
     }else if(code == KEY_DOWN) {
         //printf("left");
+        if (!(vx <= 0)) {
         vx--;
+        }
     }else if(code == KEY_RIGHT) {
         //printf("down");
+        if (!(vy <= 0)) {
         vy--;
+        }
     }else if(code == KEY_LEFT) {
         //printf("up");
-        vy++;
+        if (!(vy >= 8)) {
+        vy++
+        }
     }else if(code == KEY_ENTER) {
         //printf("push");
         Ourindex++; ;
