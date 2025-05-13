fa8ad9546920e7189b10cc027f64413edfad03d3
Arron Ye <arronye@udel.edu>
2025-05-13 22:59:40 +0000

    removed clal to h()

--- main.c
+++ main.c
@@ -79,11 +79,9 @@ int main(int argc, char *argv[]){
     for(int i=1; i<argc; i++){
         if(strcmp(argv[i], "-h")==0){
             printf(argv[0]);
-            h();
             return 0;
         } else{
             printf(argv[0]);
-            h();
             return 0;
 
         }
